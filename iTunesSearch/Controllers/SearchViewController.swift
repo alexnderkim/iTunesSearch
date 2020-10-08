dbac252e54ff19d585845a8a1d0885d8de775b7e
Alexander Kim <alexnderkim@mail.ru>
2020-10-08 20:39:42 +0700

    Add a search bar and table view

--- iTunesSearch/Controllers/SearchViewController.swift
+++ iTunesSearch/Controllers/SearchViewController.swift
@@ -8,12 +8,85 @@
 import UIKit
 
 class SearchViewController: UIViewController {
-
+    
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var searchBar: UISearchBar!
+    
+    var searhResults = [SearchResult]()
+    var hasSearched = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableView.contentInset = UIEdgeInsets(top: 54, left: 0, bottom: 0, right: 0)
     }
+    
+    
+}
 
+extension SearchViewController: UISearchBarDelegate {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.resignFirstResponder()
+        searhResults = []
+        if searchBar.text! != "Justin Bieber" {
+            for i in 0...2 {
+                let searchResult = SearchResult()
+                searchResult.name = (String(format: "Fake Result %d for '%@'", i, searchBar.text!))
+                searchResult.artistName = searchBar.text!
+                searhResults.append(searchResult)
+            }
+        }
+        hasSearched = true
+        tableView.reloadData()
+    }
+    
+    func position(for bar: UIBarPositioning) -> UIBarPosition {
+        return .topAttached
+    }
+}
 
+extension SearchViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if !hasSearched {
+            return 0
+        } else if searhResults.count == 0 {
+            return 1
+        } else {
+            return searhResults.count
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cellIdentifier = "SearchResultCell"
+        
+        var cell: UITableViewCell! = tableView.dequeueReusableCell(withIdentifier: cellIdentifier)
+        
+        if cell == nil {
+            cell = UITableViewCell(style: .subtitle, reuseIdentifier: cellIdentifier)
+        }
+        if searhResults.count == 0 {
+            cell.textLabel!.text = "(Nothing found)"
+            cell.detailTextLabel!.text = ""
+        } else {
+            let searchResult = searhResults[indexPath.row]
+            cell.textLabel!.text = searchResult.name
+            cell.detailTextLabel!.text = searchResult.artistName
+        }
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath? {
+        if searhResults.count == 0 {
+            return nil
+        } else {
+            return indexPath
+        }
+    }
+    
+    
 }
 
---
+++ iTunesSearch/Data Model/SearchResult.swift
@@ -0,0 +1,13 @@
+//
+//  SearchResult.swift
+//  iTunesSearch
+//
+//  Created by Alexander Kim on 08.10.2020.
+//
+
+import Foundation
+
+class SearchResult {
+    var name = ""
+    var artistName = ""
+}
